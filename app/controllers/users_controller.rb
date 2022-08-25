214056cd9c6e56cc0552c6ebb577ed08edfa0526
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-08-25 13:01:29 -0500

    commit tree 18a1da197ee4e081fc068070d0a3526828a94bf7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  #parent of item
+  #
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
   end
 
 end
